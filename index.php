d55cef478c5fdda2bcf4769c264691b8f48b0adc
Paul Osman <paul@eval.ca>
2011-02-17 16:56:32 -0500

    Add custom field HTML to index.php

--- index.php
+++ index.php
@@ -15,6 +15,55 @@ bind_textdomain_codeset("messages", "UTF-8");
 textdomain('messages');
 
 require_once('header.php');
+?>
+<div class="bsd-contribForm-wrap bsd-contribForm-oneCol">
+  <div class="bsd-contribForm-aboveContent"> </div>
+    <form id="contribution">
+      <table>
+        <tbody>
+          <tr>
+            <td class="contribcolumn" style="width: 300px;">
+              <table>
+                <tbody>
+                  <tr>
+                    <td class="contribheader"><?= _('Choose Your Destiny') ?></td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table class="amounts">
+                        <tbody>
+                          <tr>
+                            <td>
+                              <label class="fieldlabel"></label>
+                              <input id="q_0" type="radio" value="5" name="amount" onclick="" />
+                              <label for="q_0">
+                                <span class="radio"><?= _('$5.00 - Sign me up and use my $5 to build a Web of good') ?></span>
+                              </label>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <label class="fieldlabel"></label>
+                              <input id="q_1" type="radio" value="30" name="amount" onclick="" />
+                              <label for="q_1">
+                                <span class="radio"><?= _('$30.00 - Sign me up, use my donation to build a Web of good and send me a Mozilla t-shirt') ?></span>
+                              </label>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </form>
+</div>
+<?php
+
 require_once('footer.php');
 
 ?>
