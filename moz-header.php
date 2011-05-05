b91831970305cc261e1790246809a6193f737337
Gordon Brander <marlfoskr@gmail.com>
2011-05-05 16:46:10 -0600

    Internationalization functions for new navigation items.

--- moz-header.php
+++ moz-header.php
@@ -2,12 +2,66 @@
 	<div class="in">
 		<h1><a href="http://mozilla.org/"><?= _('Mozilla Firefox') ?></a></h1>
 		<a href="http://www.mozilla.org/" class="mozilla"><?= _('Mozilla') ?></a>
-		<nav id="nav-main">
-			<ul>
-				<li class="first"><a href="http://www.mozilla.org/about/"><?= _('Desktop') ?></a></li>
-				<li><a href="http://www.mozilla.org/community/"><?= _('Community Map') ?></a></li>
-				<li><a href="http://www.mozilla.org/projects/"><?= _('Our Projects') ?></a></li>
-				<li><a href="http://www.mozilla.org/contribute/"><?= _('Get Involved') ?></a></li>
+		<nav id="nav-main" role="navigation"> 
+			<ul role="menubar">
+				<li id="nav-main-features"  class="first"><a aria-haspopup="true" aria-owns="nav-main-features-submenu" tabindex="0" href="/en-US/firefox/features/"><?= _('Desktop'); ?></a>
+					<ul id="nav-main-features-submenu" aria-expanded="false">
+						<li class="first"><a tabindex="-1" href="/en-US/firefox/features/"><?= _('Features'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/security/"><?= _('Security'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/performance/"><?= _('Performance'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/customize/"><?= _('Customization'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/technology/"><?= _('Technology'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/video/"><?= _('Videos'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/firefox/central/"><?= _('Tour'); ?></a></li>
+						<li class="last"><a tabindex="-1" href="/en-US/firefox/channel/"><?= _('Future Releases'); ?></a></li>
+					</ul>
+				</li> 
+				<li id="nav-main-mobile" ><a aria-haspopup="true" aria-owns="nav-main-mobile-submenu" tabindex="0" href="/en-US/mobile/"><?= _('Mobile'); ?></a>
+					<ul id="nav-main-mobile-submenu" aria-expanded="false">
+						<li class="first"><a tabindex="-1" href="/en-US/mobile/"><?= _('Mobile Overview'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/mobile/download/"><?= _('Download'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/mobile/features/"><?= _('Features'); ?></a></li><li ><a tabindex="-1" href="https://addons.mozilla.org/en-US/mobile/?browse=featured"><?= _('Customize'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/mobile/sync/"><?= _('Sync'); ?></a></li>
+						<li><a tabindex="-1" href="https://developer.mozilla.org/en-US/mobile">Develop</a></li>
+						<li><a tabindex="-1" href="/en-US/mobile/getinvolved/"><?= _('Get Involved'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/mobile/faq/"><?= _('FAQ'); ?></a></li>
+						<li class="last"><a tabindex="-1" href="https://blog.mozilla.com/mobile/"><?= _('Blog'); ?></a></li>
+					</ul>
+				</li> 
+				<li id="nav-main-addons" ><a aria-haspopup="true" aria-owns="nav-main-addons-submenu" tabindex="0" href="https://addons.mozilla.org/"><?= _('Add-ons'); ?></a>
+					<ul id="nav-main-addons-submenu" aria-expanded="false">
+						<li class="first"><a tabindex="-1" href="https://addons.mozilla.org/firefox/"><?= _('Firefox Add-ons'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/featured/"><?= _('Featured Add-ons'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/extensions/"><?= _('Extensions'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/themes/"><?= _('Themes'); ?></a></li>
+						<li><a tabindex="-1" href="http://www.getpersonas.com/"><?= _('Personas'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/search-tools/"><?= _('Search Tools'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/language-tools/"><?= _('Language Support'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/firefox/collections/"><?= _('Collections'); ?></a></li>
+						<li><a tabindex="-1" href="https://addons.mozilla.org/mobile/"><?= _('Mobile Add-ons'); ?></a></li>
+						<li class="last"><a tabindex="-1" href="https://addons.mozilla.org/firefox/developers/"><?= _('Developer Hub'); ?></a></li>
+					</ul>
+				</li> 
+				<li id="nav-main-support" ><a aria-haspopup="true" aria-owns="nav-main-support-submenu" tabindex="0" href="http://support.mozilla.com/"><?= _('Support'); ?></a>
+					<ul id="nav-main-support-submenu" aria-expanded="false">
+						<li class="first"><a tabindex="-1" href="http://support.mozilla.com/en-US/kb/"><?= _('Firefox Support'); ?></a></li>
+						<li><a tabindex="-1" href="http://support.mozilla.com/mobile"><?= _('Mobile Support'); ?></a></li>
+						<li class="last"><a tabindex="-1" href="http://support.mozillamessaging.com/en-US/kb/"><?= _('Thunderbird Support'); ?></a></li>
+					</ul>
+				</li> 
+				<li id="nav-main-about"  class="last"><a aria-haspopup="true" aria-owns="nav-main-about-submenu" tabindex="0" href="/en-US/about/"><?= _('About'); ?></a>
+					<ul id="nav-main-about-submenu" aria-expanded="false">
+						<li class="first"><a tabindex="-1" href="/en-US/about/"><?= _('About Firefox'); ?></a></li>
+						<li><a tabindex="-1" href="http://www.mozilla.org/join">Join Mozilla</a></li>
+						<li><a tabindex="-1" href="/en-US/about/participate/"><?= _('Participate'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/press/"><?= _('Press Center'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/about/careers.html"><?= _('Careers'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/about/partnerships.html"><?= _('Partnerships'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/about/legal.html"><?= _('Legal'); ?></a></li>
+						<li><a tabindex="-1" href="/en-US/about/contact.html"><?= _('Contact Us'); ?></a></li>
+						<li class="last"><a tabindex="-1" href="http://blog.mozilla.com/"><?= _('Blog'); ?></a></li>
+					</ul>
+				</li>
 			</ul>
 		</nav>
 	</div>
