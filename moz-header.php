19fb0be6c0fdede9ec7b34cac4285f1e2c8a9beb
Gordon Brander <marlfoskr@gmail.com>
2011-05-05 14:19:07 -0600

    Starting to integrate header styles from Mozilla.org

--- moz-header.php
+++ moz-header.php
@@ -1,9 +1,10 @@
-<header id="moz-header">
+<header id="header">
 	<div class="in">
 		<h1><a href="http://mozilla.org/"><img id="moz-logo" src="<?= URL_BASE; ?>assets/img/mozilla-logo.png?ver=<?= VER; ?>" alt="Mozilla" width="109" height="28" /></a></h1>
-		<nav>
+		<a href="http://www.mozilla.org/" class="mozilla"><?= _('Mozilla') ?></a>
+		<nav id="nav-main">
 			<ul>
-				<li><a href="http://www.mozilla.org/about/"><?= _('About Us') ?></a></li>
+				<li class="first"><a href="http://www.mozilla.org/about/"><?= _('Desktop') ?></a></li>
 				<li><a href="http://www.mozilla.org/community/"><?= _('Community Map') ?></a></li>
 				<li><a href="http://www.mozilla.org/projects/"><?= _('Our Projects') ?></a></li>
 				<li><a href="http://www.mozilla.org/contribute/"><?= _('Get Involved') ?></a></li>
