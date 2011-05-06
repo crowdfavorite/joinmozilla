702d975ee652b5418b14a8f009cd0bd9788d1f8e
Gordon Brander <marlfoskr@gmail.com>
2011-05-06 15:58:52 -0600

    A few more fixes to the FAQ page.

--- faq.php
+++ faq.php
@@ -32,34 +32,35 @@ textdomain('messages');
         <img id="firefox-compact" src="assets/img/page-compact/firefox-logo.png" alt="<?= _('Mozilla Firefox') ?>" /> 
       </div> 
     </div><!--/in-->
-	</div><!--/act-1-->
-	<section id="act-2" class="act">
-		<div id="faq" class="in content">
-      <div class="title-card"> 
-	        <h1><?= _('Join Mozilla Support FAQ') ?></h1> 
-	    </div> 
-	      
-		  <h2><?= _('How long will I be an official supporter?') ?></h2>
-		  <p>
-		    <?= _('When you join Mozilla you will become an official supporter for a 12-month period from the date you signed up. A few weeks before the 12-month period is complete, you will receive a notification asking if you would like to join again for another year. Please know that your subscription will not auto-renew.') ?>
-		  </p>
-		  <h2><?= _('What if I change my mind?') ?></h2>
-		  <p>
-		    <?= _('You can unsubscribe from any Join Mozilla communications at any time. Please know that your donation is a one-time event, and will not auto-renew.') ?>
-		  </p>
-		  <h2><?= _('When I Join Mozilla, what happens to my data?') ?></h2>
-		  <p>
-		    <?php /* TODO: Replace 'http://mozilla.org/' with actual URL for Join Mozilla ToS. */ ?>
-		    <?= sprintf( /* L10n: both placeholders are URLs. */ _('At the point of signing up your data is being given to Mozilla (which includes the Mozilla Foundation and Corporation). We will never sell, rent, or give your information to any third-party, and will only contact you as part of this program. For more information, check out our <a href="%s">terms of service</a> and <a href="%s">privacy policy</a>.'), "http://mozilla.org/", "http://www.mozilla.org/about/policies/privacy-policy.html") ?>
-		  </p>
-		  <h2><?= _('How will my data be used?') ?></h2>
-		  <p>
-		    <?= _('By signing up for the Join Mozilla program, your data will only be used by Mozilla (and its service providers). You will only receive information Mozilla related information.') ?>
-		  </p>
-		  <h2><?= _('Help! I have a question about being part of Join Mozilla. Who do I ask?') ?></h2>
-		  <p>
-		    <?= _('Fret not! Just send an e-mail to joinmozilla@mozilla.org and we\'ll get you sorted out.') ?>
-		  </p>
+  </div><!--/act-1-->
+  <section id="act-2" class="act">
+		<div id="faq" class="in">
+		  <div class="title-card"> 
+          <h1><?= _('Join Mozilla Support FAQ') ?></h1> 
+      </div>
+      <div class="content">
+        <h2><?= _('How long will I be an official supporter?') ?></h2>
+        <p>
+          <?= _('When you join Mozilla you will become an official supporter for a 12-month period from the date you signed up. A few weeks before the 12-month period is complete, you will receive a notification asking if you would like to join again for another year. Please know that your subscription will not auto-renew.') ?>
+        </p>
+        <h2><?= _('What if I change my mind?') ?></h2>
+        <p>
+          <?= _('You can unsubscribe from any Join Mozilla communications at any time. Please know that your donation is a one-time event, and will not auto-renew.') ?>
+        </p>
+        <h2><?= _('When I Join Mozilla, what happens to my data?') ?></h2>
+        <p>
+          <?php /* TODO: Replace 'http://mozilla.org/' with actual URL for Join Mozilla ToS. */ ?>
+          <?= sprintf( /* L10n: both placeholders are URLs. */ _('At the point of signing up your data is being given to Mozilla (which includes the Mozilla Foundation and Corporation). We will never sell, rent, or give your information to any third-party, and will only contact you as part of this program. For more information, check out our <a href="%s">terms of service</a> and <a href="%s">privacy policy</a>.'), "http://mozilla.org/", "http://www.mozilla.org/about/policies/privacy-policy.html") ?>
+        </p>
+        <h2><?= _('How will my data be used?') ?></h2>
+        <p>
+          <?= _('By signing up for the Join Mozilla program, your data will only be used by Mozilla (and its service providers). You will only receive information Mozilla related information.') ?>
+        </p>
+        <h2><?= _('Help! I have a question about being part of Join Mozilla. Who do I ask?') ?></h2>
+        <p>
+          <?= _('Fret not! Just send an e-mail to joinmozilla@mozilla.org and we\'ll get you sorted out.') ?>
+        </p>
+      </div><!--/content-->
 		</div><!--/faq-->
 	</section><!--/act-2-->
 	<?php
